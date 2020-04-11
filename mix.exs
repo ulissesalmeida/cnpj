1c7cc8fdcf219be418d8e67fe71319c5e53cc794
Ulisses Almeida <anizark@gmail.com>
2020-04-11 18:41:47 +0300

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -9,6 +9,13 @@ defmodule CNPJ.MixProject do
       package: package(),
       description: description(),
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -37,7 +44,8 @@ defmodule CNPJ.MixProject do
   defp deps do
     [
       {:ex_doc, only: :dev, runtime: false},
-      {:credo, "~> 1.3", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
