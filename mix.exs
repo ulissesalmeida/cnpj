69884ec44518e4b4b0cf40f7227aff4bed109014
Ulisses Almeida <anizark@gmail.com>
2020-04-11 18:32:59 +0300

    Add credo

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule CNPJ.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, only: :dev, runtime: false}
+      {:ex_doc, only: :dev, runtime: false},
+      {:credo, "~> 1.3", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
